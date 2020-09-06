45d66eaa7857b1f4c9a6530cf22f98b14f3d5fab
Érick Mazzocco <erick.mazzocco@elogroup.com.br>
2020-09-06 13:13:50 -0300

    errors

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,33 +1,67 @@
-import React from 'react';
-import { Container, Content, Background } from './styles';
+import React, { useCallback, useRef } from 'react';
+import { FormHandles } from '@unform/core';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErros';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+import { Container, Content, Background } from './styles';
 
-      <form>
-        <h1>Faça seu cadastro</h1>
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-        <Input icon={FiUser} name="name" placeholder="Nome" />
-        <Input icon={FiMail} name="email" placeholder="E-mail" />
-        <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
+      const schema = Yup.object().shape({
+        name: Yup.string()
+          .required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string()
+          .min(6, 'No mínimo 6 dígitos'),
+      })
 
-      <a href="login">
-        <FiArrowLeft />
-          Voltar para Logon
-      </a>
+      await schema.validate(data, {
+        abortEarly: false,
+      });
 
-    </Content>
-  </Container>
-)
+    } catch (err) {
+
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+          <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+            Voltar para Logon
+        </a>
+
+      </Content>
+    </Container>
+  )
+}
 
 export default SignUp;
